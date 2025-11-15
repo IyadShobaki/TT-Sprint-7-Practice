636c9c9189899eafe1da178984c2dc044e1881ca
Iyad <iyadshobaki82@gmail.com>
2025-11-15 09:49:12 -0500

    Add Card class with a function to return the card template

--- shopping-site/dist/script.dev.js
+++ shopping-site/dist/script.dev.js
@@ -1,1 +1,30 @@
 "use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Card =
+/*#__PURE__*/
+function () {
+  function Card(title, description, price, image) {
+    _classCallCheck(this, Card);
+
+    this._title = title;
+    this._description = description;
+    this._price = price;
+    this._image = image;
+  }
+
+  _createClass(Card, [{
+    key: "_getTemplate",
+    value: function _getTemplate() {
+      var cardElement = document.querySelector(".horizontal-card").content.querySelector(".card").cloneNode(true);
+      return cardElement;
+    }
+  }]);
+
+  return Card;
+}();
